381ee4bbb8514b12e34a4aa8f63c2fa14fd1e4e1
Effanuel <etajuozas@gmail.com>
2019-10-11 17:34:38 +0300

    added files and initial functionality

---
+++ calculator/script.js
@@ -0,0 +1,52 @@
+const appendValue = element => {
+  var value = $(element).val();
+  $('#output').append(value);
+};
+
+const popValue = () => {
+  $('#output').text((index, value) => {
+    return value.substr(0, value.length - 1);
+  });
+};
+
+const replaceValue = element => {
+  popValue();
+  appendValue(element);
+};
+
+$(function() {
+  $('button[id^="input"]').click(function() {
+    appendValue(this);
+  });
+
+  $('button[id^="equal"]').click(function() {
+    const current = $('#output').text();
+    const answer = eval(current);
+    $('#output').text(answer);
+  });
+
+  $('button[id^="clear"]').click(function() {
+    $('#output').text('');
+  });
+
+  $('button[id^="pop"]').click(function() {
+    popValue();
+  });
+  $('button[id^="symbol"]').click(function() {
+    // if (this.id == 'pop') {
+    //   popValue();
+    // }
+    const array = ['+', '-', '/', '*'];
+    const lastElem = $('#output')
+      .text()
+      .slice(-1);
+
+    if (array.includes(lastElem) && lastElem != $(this).val()) {
+      replaceValue(this);
+      return;
+    } else if (array.includes(lastElem) && lastElem == $(this).val()) {
+      return;
+    }
+    appendValue(this);
+  });
+});
