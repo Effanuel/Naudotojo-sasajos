6fdf5c7a58381a080cc42c31d09a6333fa3fd852
Effanuel <etajuozas@gmail.com>
2019-10-22 14:06:50 +0300

    added root/square/pi/factorial

--- calculator/script.js
+++ calculator/script.js
@@ -1,10 +1,10 @@
 const appendValue = element => {
   var value = $(element).val();
-  $("#output").append(value);
+  $('#output').append(value);
 };
 
 const popValue = () => {
-  $("#output").text((index, value) => {
+  $('#output').text((index, value) => {
     return value.substr(0, value.length - 1);
   });
 };
@@ -15,70 +15,145 @@ const replaceValue = element => {
 };
 
 function equal() {
-  const current = $("#output").text();
+  const current = $('#output').text();
   const answer = eval(current);
-  $("#output").text(answer);
+  $('#output').text(answer);
 }
 
-function root() {
-  const current = $("#output").text();
+function root(power) {
+  const current = $('#output').text();
   try {
     // Is a positve number and is it a number (handles symbols)
-    if (current < 0 || !parseFloat(current)) throw "Error";
-    const answer = Math.sqrt(current);
-    $("#output").text(answer);
+    if (eval(current) < 0 || !parseFloat(current)) throw 'Error';
+    //Eval before taking root so sqrt(9*9) is valid for example
+    const answer = Math.pow(eval(current), 1 / power);
+    $('#output').text(answer);
   } catch (err) {
-    $("#output").text("");
-    $("#error")
-      .text(err)
-      .fadeOut(2000);
+    $('#output').text(err);
   }
 }
 
-function findSymbolIndexMax(string) {
-  const symbolsArr = ["+", "-", "/", "*"];
+function fact(num) {
+  if (eval(num) < 0) throw 'Error';
+  else if (eval(num) == 0) return 1;
+  else {
+    return num * fact(num - 1);
+  }
+}
 
+function factorial() {
+  const current = $('#output').text();
+  try {
+    // Is a positve number and is it a number (handles symbols)
+    let val = fact(current);
+    //Eval before taking root so sqrt(9*9) is valid for example
+
+    $('#output').text(val);
+  } catch (err) {
+    $('#output').text(err);
+  }
+}
+
+function isLastElementSymbol(array = ['+', '-', '/', '*', '.']) {
+  const lastElem = $('#output')
+    .text()
+    .slice(-1);
+  return array.includes(lastElem);
+}
+
+function PI() {
+  if ($('#output').text().length >= 20) return;
+
+  if (isLastElementSymbol(['+', '-', '/', '*'])) {
+    $('#output').append(3.14);
+    return;
+  } else if (
+    (array.includes(lastElem) && lastElem == $(this).val()) ||
+    $('#output').text() == ''
+  ) {
+    return;
+  }
+}
+
+function getIndicesOf(searchStr, str, caseSensitive = false) {
+  var searchStrLen = searchStr.length;
+  if (searchStrLen == 0) {
+    return [];
+  }
+  var startIndex = 0,
+    index,
+    indices = [];
+  if (!caseSensitive) {
+    str = str.toLowerCase();
+    searchStr = searchStr.toLowerCase();
+  }
+  while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+    indices.push(index);
+    startIndex = index + searchStrLen;
+  }
+  return indices;
+}
+
+function findSymbolIndexMax(string) {
+  const symbolsArr = ['+', '-', '/', '*'];
+  // const current = $('#output').text();
+
+  // current.forEach(function(item, i, text) {
+  //   symbolsArr.forEach(function(symbol, j, symbols) {
+  //     text[i] = current.indexOf(item);
+  //   });
+  // });
+  let allOccurences = [];
+  let occurences = [];
   symbolsArr.forEach(function(item, index, arr) {
-    arr[index] = string.indexOf(item);
+    occurences = getIndicesOf(item, string);
+    allOccurences.push(...occurences);
   });
-  return Math.max(...symbolsArr);
+  return Math.max(...allOccurences);
 }
 
 $(function() {
   $('button[id^="input"]').click(function() {
-    if ($("#output").text().length >= 20) return;
+    if ($('#output').text().length >= 20) return;
     appendValue(this);
   });
   $('button[id^="dot"]').click(function() {
-    if ($("#output").text().length >= 20) return;
-    const string = $("#output").text();
-    const i = string.indexOf(".");
+    if ($('#output').text().length >= 20) return;
+    const string = $('#output').text();
 
-    const symbolIndexMax = findSymbolIndexMax(string);
+    const i = Math.max(...getIndicesOf('.', string));
 
-    if (symbolIndexMax >= i) appendValue(this);
+    const symbolIndexMax =
+      findSymbolIndexMax(string) < -1 ? -1 : findSymbolIndexMax(string);
+
+    if (symbolIndexMax >= i) {
+      if (!isLastElementSymbol(['1', '2', '3', '4', '5', '6', '7', '8', '9'])) {
+        $('#output').append('0.');
+      } else {
+        appendValue(this);
+      }
+    }
   });
 
   $('button[id^="clear"]').click(function() {
-    $("#output").text("");
+    $('#output').text('');
   });
 
   $('button[id^="pop"]').click(function() {
     popValue();
   });
   $('button[id^="symbol"]').click(function() {
-    // if (this.id == 'pop') {
-    //   popValue();
-    // }
-    const array = ["+", "-", "/", "*"];
-    const lastElem = $("#output")
+    const lastElem = $('#output')
       .text()
       .slice(-1);
 
-    if (array.includes(lastElem) && lastElem != $(this).val()) {
+    if (isLastElementSymbol() && lastElem != $(this).val()) {
       replaceValue(this);
       return;
-    } else if (array.includes(lastElem) && lastElem == $(this).val()) {
+    } else if (
+      (isLastElementSymbol() && lastElem == $(this).val()) ||
+      $('#output').text() == ''
+    ) {
       return;
     }
     appendValue(this);
