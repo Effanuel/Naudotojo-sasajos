27b1a3ddedc5f2c1e868255eda5154863893bae1
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-11-14 22:41:03 +0200

    Final tic tac toe version

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -25,10 +25,15 @@ class TicTacToe {
       setSymbol(i, j, symbol);
 
       const player_state = this.checkState();
-      if (player_state == "X" || player_state == "O")
-        alert(`${player_state} WON`);
-      else if (player_state == "TIE") alert("TIE");
-      else this.updatePlayerMove();
+      if (player_state == "X" || player_state == "O") {
+        $("#modal").css({ display: "block" });
+        $("#winner").text(`Player ${player_state} won!`);
+      } else if (player_state == "TIE") {
+        $("#modal").css({ display: "block" });
+        $("#winner").text("TIE");
+      } else {
+        this.updatePlayerMove();
+      }
     }
   }
   checkState() {
@@ -41,7 +46,7 @@ class TicTacToe {
     const ver2 = [state[0][1], state[1][1], state[2][1]];
     const ver3 = [state[0][2], state[1][2], state[2][2]];
     const diag1 = [state[0][0], state[1][1], state[2][2]];
-    const diag2 = [state[0][2], state[1][2], state[0][2]];
+    const diag2 = [state[0][2], state[1][1], state[2][0]];
 
     const spread_state = [...state[0], ...state[1], ...state[2]];
     if (!spread_state.includes(" ")) {
@@ -60,13 +65,8 @@ class TicTacToe {
     )
       return this.player_move;
 
-    return "."; // Continue game
+    return ".";
   }
-
-  //
-  //
-  //
-
   resetGame() {
     this.state = [
       [" ", " ", " "],
@@ -81,139 +81,20 @@ class TicTacToe {
       }
     }
   }
-
-  max() {
-    // POSSIBLE VALUES
-    // -1 LOSE
-    // 0 TIE
-    // 1 WIN
-
-    let maxv = -2,
-      px,
-      py,
-      m,
-      min_return;
-
-    const player_state = this.checkState();
-
-    if (player_state == "X") return [-1, 0, 0];
-    else if (player_state == "O") return [1, 0, 0];
-    else if (player_state == "TIE") return [0, 0, 0];
-
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (this.state[i][j] == " ") {
-          this.state[i][j] = "O";
-          min_return = this.min();
-          m = min_return[0];
-          px = min_return[1];
-          py = min_return[2];
-
-          if (m > maxv) {
-            maxv = m;
-            px = i;
-            py = j;
-          }
-          this.state[i][j] = " ";
-        }
-      }
-    }
-    return [maxv, px, py];
-  }
-  min() {
-    // POSSIBLE VALUES
-    // -1 LOSE
-    // 0 TIE
-    // 1 WIN
-
-    let minv = 2,
-      qx,
-      qy,
-      m,
-      max_return;
-
-    const player_state = this.checkState();
-
-    if (player_state == "X") return [-1, 0, 0];
-    else if (player_state == "O") return [1, 0, 0];
-    else if (player_state == "TIE") return [0, 0, 0];
-
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (this.state[i][j] == " ") {
-          this.state[i][j] = "X";
-          max_return = this.max();
-          m = max_return[0];
-          qx = max_return[1];
-          qy = max_return[2];
-
-          if (m < minv) {
-            minv = m;
-            qx = i;
-            qy = j;
-          }
-          this.state[i][j] = " ";
-        }
-      }
-    }
-    return [minv, qx, qy];
-  }
-
-  play() {
-    let m, qx, qy, px, py, min_return, max_return;
-    while (true) {
-      setTimeout(function() {
-        console.log("TIMEOUT");
-      }, 5000);
-      $("[id^=tile]").click(function() {
-        px = this.id[4];
-        px = this.id[5];
-        qx = px;
-        qy = px;
-        break;
-      });
-    }
-    // // while (true) {
-    //   this.resetGame();
-    //   const player_state = this.checkState();
-    //   if (player_state == "X") alert("PLAYER X WON");
-    //   else if (player_state == "O") alert("PLAYER Y WON");
-    //   else if (player_state == "TIE") alert("TIE");
-    //   if (this.player_move == "X") {
-    //     min_return = this.min();
-    //     m = min_return[0];
-    //     qx = min_return[1];
-    //     qy = min_return[2];
-    //     while (true) {
-    //       console.log("hello");
-    //       $("[id^=tile]").click(function() {
-    //         px = this.id[4];
-    //         px = this.id[5];
-    //         qx = px;
-    //         qy = px;
-    //         break;
-    //       });
-    //     }
-    //   } else {
-    //     max_return = this.max();
-    //     m = max_return[0];
-    //     px = max_return[1];
-    //     py = max_return[2];
-    //     setSymbol(px, py, "O");
-    //     this.updatePlayerMove();
-    //   }
-    // }
-  }
 }
 
-tic = new TicTacToe();
-tic.play();
+// tic = new TicTacToe();
+// tic.play();
 
-// $(function() {
-//   tic = new TicTacToe();
-//   $("[id^=tile]").click(function() {
-//     const i = this.id[4];
-//     const j = this.id[5];
-//     tic.setState(i, j, tic.player_move);
-//   });
-// });
+$(function() {
+  tic = new TicTacToe();
+  $("[id^=tile]").click(function() {
+    const i = this.id[4];
+    const j = this.id[5];
+    tic.setState(i, j, tic.player_move);
+  });
+  $("#play").click(function() {
+    $("#modal").css({ display: "none" });
+    tic.resetGame();
+  });
+});
