d80b712c24f7c505c9f838d7bd86741d9b5f7c6b
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-11-27 19:23:06 +0200

    Generates table

--- dynamic tables/script.js
+++ dynamic tables/script.js
@@ -1,81 +1,77 @@
-class GenerateTable2 {
-  constructor(name, rows, cols, col_names, size) {
+class Table {
+  constructor(name = "Table name", rows, cols, size) {
     this.name = name;
-    this.rows = rows;
-    this.cols = cols;
-    this.col_names = col_names;
+    this.rows = parseInt(rows);
+    this.cols = parseInt(cols);
+    this.size = size;
+    this.table_div;
+  }
+
+  setParameters(name, rows, cols, size = 10) {
+    this.name = name;
+    this.rows = parseInt(rows);
+    this.cols = parseInt(cols);
     this.size = size;
   }
 
   drawTable() {
-    var min = 1;
-    var max = 10;
     var table_div = document.getElementById("dvTable");
+    // Clear contents
+    table_div.innerHTML = "";
+    // Add table name
+    const table_name = document.createElement("div");
+    table_name.setAttribute("class", "tableName");
+    table_name.innerText = this.name;
+    table_div.appendChild(table_name);
 
-    // creates a <table> element
+    // create a <table> element
     var table = document.createElement("table");
 
     // creating rows
-    for (var r = 0; r < this.rows; r++) {
+    for (var r = 0; r < this.rows + 1; r++) {
       var row = document.createElement("tr");
 
       // create cells in row
-      for (var c = 0; c < this.cols; c++) {
-        var col = document.createElement("td");
-        var cellText = document.createTextNode(
-          Math.floor(Math.random() * (max - min + 1)) + min
-        );
-        col.appendChild(cellText);
+      var cellText;
+      for (var c = 0; c < this.cols + 1; c++) {
+        var col = document.createElement("input");
+        col.setAttribute("class", "cell");
+
+        // FIRST COLUMN SETTINGS
+        if (c == 0) {
+          col.style.backgroundColor = "lightgrey";
+          col.value = r;
+          if (r == 0) col.value = "Nr.";
+        }
+        //======================
+
+        // FIRST ROW SETTINGS
+        if (r == 0) {
+          col.style.backgroundColor = "darkgrey";
+        }
+        //======================
         row.appendChild(col);
       }
-
       table.appendChild(row); // add the row to the end of the table body
     }
 
-    table_div.appendChild(tbl);
+    table_div.appendChild(table);
+    this.table_div = table_div;
   }
 }
+const table = new Table();
 
-function GenerateTable() {
-  //Build an array containing Customer records.
-  var customers = new Array();
-  customers.push(["Customer Id", "Name", "Country"]);
-  customers.push([1, "John Hammond", "United States"]);
-  customers.push([2, "Mudassar Khan", "India"]);
-  customers.push([3, "Suzanne Mathews", "France"]);
-  customers.push([4, "Robert Schidner", "Russia"]);
+const generateTable = () => {
+  console.log("LOG");
+  const table_name = document.getElementById("tname").value;
+  const rows = document.getElementById("frows").value;
+  const cols = document.getElementById("fcols").value;
 
-  //Create a HTML Table element.
-  var table = document.createElement("TABLE");
-  table.border = "1";
-
-  //Get the count of columns.
-  var columnCount = customers[0].length;
-
-  //Add the header row.
-  var row = table.insertRow(-1);
-  for (var i = 0; i < columnCount; i++) {
-    var headerCell = document.createElement("TH");
-    headerCell.innerHTML = customers[0][i];
-    row.appendChild(headerCell);
-  }
-
-  //Add the data rows.
-  for (var i = 1; i < customers.length; i++) {
-    row = table.insertRow(-1);
-    for (var j = 0; j < columnCount; j++) {
-      var cell = row.insertCell(-1);
-      cell.innerHTML = customers[i][j];
-    }
-  }
-
-  var dvTable = document.getElementById("dvTable");
-  dvTable.innerHTML = "";
-  dvTable.appendChild(table);
-}
-
-const gen = () => {
   // GenerateTable();
-  let table = new GenerateTable2();
+  table.setParameters(table_name, rows, cols);
   table.drawTable();
+};
+
+const createTable = () => {
+  //check if it is generated
 };
